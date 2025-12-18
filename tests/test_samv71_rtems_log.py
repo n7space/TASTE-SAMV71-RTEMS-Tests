f9e7880cf27da68161d6f8f0bd8145951705f003
Filip Florczyk <filip.florczyk@n7space.com>
2025-12-18 09:09:02 +0100

    Added working activation log test

---
+++ tests/test_samv71_rtems_log.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import common
+import time
+import os
+from pygdbmi.gdbcontroller import GdbController
+
+
+def test_samv71_rtems_log():
+    common.do_clean_build("samv71-rtems-log/TEST-SAMV71-ACTIVATION-LOG")
+    remote_gdb_server = os.getenv("SAMV71_REMOTE_GDBSERVER", default="127.0.0.1")
+    
+    build = common.do_build("samv71-rtems-log/TEST-SAMV71-ACTIVATION-LOG", ["samv71", "debug"])
+    stderr = build.stderr.decode("utf-8")
+    assert build.returncode == 0, f"Compilation errors: \n{stderr}"
+
+    common.run_verification_project(remote_gdb_server, 'samv71-rtems-log/TEST-SAMV71-ACTIVATION-LOG/work/binaries/partition_1', 'testfunction.c', '66')
+
+if __name__ == "__main__":
+    test_samv71_rtems_log()
