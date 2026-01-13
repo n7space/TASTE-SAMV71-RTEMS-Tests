ef4c3b13119d0507c5258c0596cf027ae80cbcd3
Rafał Babski <rbabski@n7space.com>
2026-01-13 12:13:29 +0100

    test samv71-rtems-can-static

--- tests/test_samv71_rtems_can.py
+++ tests/test_samv71_rtems_can.py
@@ -39,5 +39,38 @@ def test_samv71_rtems_can_simple():
     assert not errors, "\n".join(errors)
 
 
+def test_samv71_rtems_can_static():
+    remote_gdb_server = os.getenv("SAMV71_REMOTE_GDBSERVER", default="127.0.0.1")
+    build = common.do_build("samv71-rtems-can-static", ["deploymentview", "debug"])
+    stderr = build.stderr.decode("utf-8")
+    assert build.returncode == 0, f"Compilation errors: \n{stderr}"
+
+    gdbmi = GdbController(command=["gdb-multiarch", "--interpreter=mi2"])
+    try:
+        gdbmi.write(f"target extended-remote {remote_gdb_server}")
+        gdbmi.write("file samv71-rtems-can-static/work/binaries/partition_1")
+        gdbmi.write("monitor reset")
+        gdbmi.write("load")
+        gdbmi.write("continue")
+
+        expected = [
+            "  can1  RX - -  089   [2]  00 00",
+            "  can1  RX - -  089   [2]  00 01",
+            "  can1  RX - -  089   [2]  00 02",
+            "  can1  RX - -  089   [2]  00 03",
+            "  can1  RX - -  089   [2]  00 04",
+        ]
+
+        errors = common.do_execute(
+            "samv71-rtems-can-static",
+            expected,
+            test_exe="test_samv71_rtems_can_static.sh",
+        )
+    finally:
+        gdbmi.exit()
+    assert not errors, "\n".join(errors)
+
+
 if __name__ == "__main__":
     test_samv71_rtems_can_simple()
+    test_samv71_rtems_can_static()
